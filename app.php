ee91f111214ed189510cdbb5680bba01fe38928a
tanvir <aiub.tanvir@gmail.com>
2022-12-01 00:19:00 +0600

    second commit
    Signed-off-by: tanvir <aiub.tanvir@gmail.com>

--- app.php
+++ app.php
@@ -12,18 +12,18 @@
         font-size: 16px;
         color: #eee;
     }
-    
+
     h1,
     h2 {
         text-transform: uppercase;
         font-weight: 400;
         color: aliceblue;
     }
-    
+
     h2 {
         margin: 0 0 0 8px;
     }
-    
+
     .main-block {
         display: flex;
         flex-direction: column;
@@ -34,36 +34,36 @@
         background: url("https://www.w3docs.com//uploads/media/default/0001/01/b5edc1bad4dc8c20291c8394527cb2c5b43ee13c.jpeg") no-repeat center;
         background-size: cover;
     }
-    
+
     .left-part,
     form {
         padding: 25px;
     }
-    
+
     .left-part {
         text-align: center;
     }
-    
+
     .fa-graduation-cap {
         font-size: 72px;
     }
-    
+
     form {
         background: rgba(0, 0, 0, 0.7);
         border-radius: 10px;
     }
-    
+
     .title {
         display: flex;
         align-items: center;
         margin-bottom: 20px;
     }
-    
+
     .info {
         display: flex;
         flex-direction: column;
     }
-    
+
     input,
     select {
         padding: 5px;
@@ -72,33 +72,33 @@
         border: none;
         border-bottom: 1px solid #eee;
     }
-    
+
     input::placeholder {
         color: #eee;
     }
-    
+
     option:focus {
         border: none;
     }
-    
+
     option {
         background: black;
         border: none;
     }
-    
+
     .checkbox input {
         margin: 0 10px 0 0;
         vertical-align: middle;
     }
-    
+
     .checkbox a {
         color: #26a9e0;
     }
-    
+
     .checkbox a:hover {
         color: #85d6de;
     }
-    
+
     .btn-item,
     button {
         padding: 10px 5px;
@@ -111,36 +111,40 @@
         font-weight: 400;
         color: #fff;
     }
-    
+
     .btn-item {
         display: inline-block;
         margin: 20px 5px 0;
         padding: 5px;
         border-radius: 10px;
     }
-    
+
     button {
         width: 100%;
     }
-    
+
     button:hover,
     .btn-item:hover {
         background: #85d6de;
     }
+
     .error {
         color: red;
         margin-bottom: 15px;
     }
-    
+
     @media (min-width: 568px) {
+
         html,
         body {
             height: 100%;
         }
+
         .main-block {
             flex-direction: row;
             height: calc(100% - 50px);
         }
+
         .left-part,
         form {
             flex: 1;
@@ -198,7 +202,7 @@
             <div class="checkbox">
                 <input type="checkbox" name="checkbox"><span>I agree to the <a href="#">Privacy Policy</a></span>
             </div>
-            <button type="submit" href="/">Submit</button>
+            <button id="submit-btn" type="submit" href="/">Submit</button>
         </form>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
@@ -209,135 +213,147 @@
         });
     </script>
     <script>
-        function getBranches(){
-            $.get("http://127.0.0.1:8000/api/get-branches",function(data,status){
+        function getBranches() {
+            $.get("http://127.0.0.1:8000/api/get-branches", function(data, status) {
                 //console.log(data);
-                $.each(data, function (i, branch) {
+                $.each(data, function(i, branch) {
                     $('#branch_id').append('<option value="' + branch.id + '">' + branch.branch_name + '</option>');
                     console.log(branch.id);
                 });
             });
         }
-        function getCounselor(){
+
+        function getCounselor() {
             var branch_id = $('#branch_id').val();
-            if(branch_id===""){
+            if (branch_id === "") {
                 return false;
             }
-            $.get("http://127.0.0.1:8000/api/get-counsellor-by-branch-for-appointment/"+branch_id,function(data,status){
+            $.get("http://127.0.0.1:8000/api/get-counsellor-by-branch-for-appointment/" + branch_id, function(data, status) {
                 //console.log(data);
-                if(data['result']['key']===101){
+                if (data['result']['key'] === 101) {
                     alert(data['result']['val']);
                 }
-                if(data['result']['key']===200){
+                if (data['result']['key'] === 200) {
                     $("#counselor_id").empty()
                     //console.log(data['result']['val']);
                     $('#counselor_id').append('<option value="">--Select Consultant--</option>');
-                    $.each(data.result.val, function (i, counselor) {
-                    $('#counselor_id').append('<option value="' + counselor.id + '">' + counselor.counselor_name + '</option>');
-                  });
+                    $.each(data.result.val, function(i, counselor) {
+                        $('#counselor_id').append('<option value="' + counselor.id + '">' + counselor.counselor_name + '</option>');
+                    });
                 }
-                
+
             });
         }
     </script>
     <script>
         $(document).ready(function() {
-       $('#formid').validate({
-           rules: {
-                branch_id : {
-                    required: true
+            $('#formid').validate({
+                rules: {
+                    branch_id: {
+                        required: true
+                    },
+                    counselor_id: {
+                        required: true
+                    },
+                    name: {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    phone: {
+                        required: true
+                    },
+                    date_of_bith: {
+                        required: true
+                    },
+                    is_first_visit: {
+                        required: true
+                    },
+                    description: {
+                        required: true
+                    },
+                    appointment_date: {
+                        required: true
+                    },
                 },
-                counselor_id : {
-                    required: true
+                messages: {
+                    branch_id: {
+                        required: "Please Select Branch"
+                    },
+                    counselor_id: {
+                        required: "Please Select Counselor"
+                    },
+                    email: {
+                        required: "Email Field Is Required!",
+                        email: "Email Must Be Valid Email Address!"
+                    },
+                    name: {
+                        required: "Full Name Field Is Required!",
+                    },
+                    phone: {
+                        required: "Phone Number Field Is Required!",
+                    },
+                    date_of_bith: {
+                        required: "Date of Birth Field Is Required!",
+                    },
+                    is_first_visit: {
+                        required: "Please Select Your Visit Status!",
+                    },
+                    description: {
+                        required: "Message Field is Required!",
+                    },
+                    appointment_date: {
+                        required: "Appointment Date Time Field is Required!",
+                    }
                 },
-                name : {
-                    required:true
-                },
-                email : {
-                    required: true,
-                    email: true
-                },
-                phone : {
-                    required: true
-                },
-                date_of_bith : {
-                    required: true
-                },
-                is_first_visit : {
-                    required: true
-                },
-                description : {
-                    required: true
-                },
-                appointment_date : {
-                    required: true
-                },
-           },
-           messages: {
-            branch_id: {
-                required: "Please Select Branch"
-            },
-            counselor_id: {
-                required: "Please Select Counselor"
-            },
-            email: {
-                required: "Email Field Is Required!",
-                email: "Email Must Be Valid Email Address!"
-            },
-            name: {
-                required: "Full Name Field Is Required!",
-            },
-            phone: {
-                required: "Phone Number Field Is Required!",
-            },
-            date_of_bith: {
-                required: "Date of Birth Field Is Required!",
-            },
-            is_first_visit:{
-                required: "Please Select Your Visit Status!",
-            },
-            description:{
-                required: "Message Field is Required!",
-            },
-            appointment_date:{
-                required: "Appointment Date Time Field is Required!",
-            }
-           },
-           submitHandler: function(form) {
-            var dataval = {
-                branch_id: $('#branch_id').val(),
-                counselor_id: $('#counselor_id').val(),
-                email: $('#email').val(),
-                name: $('#name').val(),
-                phone: $('#phone').val(),
-                date_of_bith: $('#date_of_bith').val(),
-                is_first_visit: $('#is_first_visit').val(),
-                description: $('#description').val(),
-                appointment_date: $('#appointment_date').val(),
-            };
-            var korte = JSON.stringify(dataval);
-            //alert(JSON.stringify(data));
-            //console.log(JSON.stringify(data));
-            $.get("http://127.0.0.1:8000/api/book-appointment-submit/"+korte,function(data,status){
-                console.log(data);
+                submitHandler: function(form) {
+                    $('#submit-btn').prop('disabled', true);
+                    var dataval = {
+                        branch_id: $('#branch_id').val(),
+                        counselor_id: $('#counselor_id').val(),
+                        email: $('#email').val(),
+                        name: $('#name').val(),
+                        phone: $('#phone').val(),
+                        date_of_bith: $('#date_of_bith').val(),
+                        is_first_visit: $('#is_first_visit').val(),
+                        description: $('#description').val(),
+                        appointment_date: $('#appointment_date').val(),
+                    };
+                    var formdata = JSON.stringify(dataval);
+                    //alert(JSON.stringify(data));
+                    //console.log(JSON.stringify(data));
+                    $.get("http://127.0.0.1:8000/api/book-appointment-submit/" + formdata, function(data, status) {
+                        console.log(data);
+                        if (data['result']['key'] === 101) {
+                            $('#submit-btn').prop('disabled', false);
+                            alert(data['result']['val']);
+                        }
+                        if (data['result']['key'] === 200) {
+                            $('#submit-btn').prop('disabled', false);
+                            $("#formid")[0].reset();
+                        }
+
+                    });
+                    //    var action="http://127.0.0.1:8000/api/book-appointment-submit";
+                    //    //alert('hello world');
+                    //    $.ajax({
+                    //        url : action,
+                    //        data:$(form).serialize(),
+                    //        type : 'get',
+                    //        success : function(result){
+                    //            // alert(result.url);
+                    //            console.log(result);
+                    //        },
+                    //        error:function (response) {
+                    //            console.log(response);
+                    //        }
+                    //    });
+                }
             });
-            //    var action="http://127.0.0.1:8000/api/book-appointment-submit";
-            //    //alert('hello world');
-            //    $.ajax({
-            //        url : action,
-            //        data:$(form).serialize(),
-            //        type : 'get',
-            //        success : function(result){
-            //            // alert(result.url);
-            //            console.log(result);
-            //        },
-            //        error:function (response) {
-            //            console.log(response);
-            //        }
-            //    });
-           }
-       });
-   });
+        });
     </script>
 </body>
+
 </html>
